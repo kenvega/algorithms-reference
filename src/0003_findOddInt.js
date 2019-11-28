2a82fc2e0eac23f1c448aba072e0ed419de21888
Ken Vega <ken.vega.g@gmail.com>
2019-11-27 22:47:53 -0500

    start findOddInt solution

---
+++ src/0003_findOddInt.js
@@ -0,0 +1,13 @@
+// Given an array, find the int that appears an odd number of times.
+// There will always be only one integer that appears an odd number of times.
+
+function findOdd(array) {
+  // counters for every time i find a number
+  // can store them in an object with keys as the numbers and values as the number of times they have appeared
+  // then just is a find in that
+
+  return 0;
+}
+
+console.log('findOdd', findOdd([1, 2, 1, 1, 2, 4])); // 1
+console.log('findOdd', findOdd([1, 2, 1, 1, 1, 2, 4])); // 4
