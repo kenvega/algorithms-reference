9303cadf2c066b9337ac8bd86e9e9ec201ba261b
Ken Vega <ken.vega.g@gmail.com>
2019-12-01 18:45:36 -0500

    update find anagram

--- src/0009_findAnagram.js
+++ src/0009_findAnagram.js
@@ -1,3 +1,6 @@
+// level : 5kyu
+// https://www.codewars.com/kata/523a86aa4230ebb5420001e1
+
 // Write a function that will find all the anagrams of a word from a list. You will be given two inputs a word and an array with words. You should return an array of all the anagrams or an empty array if there are none. For example:
 
 // anagrams('abba', ['aabb', 'abcd', 'bbaa', 'dada']) => ['aabb', 'bbaa']
