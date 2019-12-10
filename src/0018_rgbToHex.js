5c27c37ad0e72e947e22e6ec943afc548c8529af
Ken Vega <ken.vega.g@gmail.com>
2019-12-09 20:30:51 -0500

    add rgb to hex

---
+++ src/0018_rgbToHex.js
@@ -0,0 +1,23 @@
+// The rgb() method is incomplete. Complete the method so that passing in RGB decimal values will result in a hexadecimal representation being returned. The valid decimal values for RGB are 0 - 255. Any (r,g,b) argument values that fall out of that range should be rounded to the closest valid value.
+
+// The following are examples of expected output values:
+
+// rgb(255, 255, 255) // returns FFFFFF
+// rgb(255, 255, 300) // returns FFFFFF
+// rgb(0,0,0) // returns 000000
+// rgb(148, 0, 211) // returns 9400D3
+
+function rgb(r, g, b) {
+  return [r, g, b].reduce((acc, cur) => {
+    if (cur > 255) return `${acc}FF`;
+    if (cur <= 0) return `${acc}00`;
+    cur = cur.toString(16).toUpperCase();
+    if (cur.length === 1) cur = 0 + cur;
+    return `${acc}${cur}`;
+  }, '');
+}
+
+console.log(rgb(0, 0, 0)); // '000000'
+console.log(rgb(0, 0, -20)); // '000000'
+console.log(rgb(300, 255, 255)); // 'FFFFFF'
+console.log(rgb(173, 255, 47)); // 'ADFF2F'
