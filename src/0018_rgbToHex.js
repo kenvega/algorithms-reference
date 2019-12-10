552133b2cac6a43b28f01f0444a5b914c9438735
Ken Vega <ken.vega.g@gmail.com>
2019-12-10 10:21:57 -0500

    update rgb to hex

--- src/0018_rgbToHex.js
+++ src/0018_rgbToHex.js
@@ -1,3 +1,6 @@
+// level 5kyu
+// https://www.codewars.com/kata/513e08acc600c94f01000001
+
 // The rgb() method is incomplete. Complete the method so that passing in RGB decimal values will result in a hexadecimal representation being returned. The valid decimal values for RGB are 0 - 255. Any (r,g,b) argument values that fall out of that range should be rounded to the closest valid value.
 
 // The following are examples of expected output values:
@@ -6,6 +9,8 @@
 // rgb(255, 255, 300) // returns FFFFFF
 // rgb(0,0,0) // returns 000000
 // rgb(148, 0, 211) // returns 9400D3
+
+// lesson: toString() can transform a number to hex format
 
 function rgb(r, g, b) {
   return [r, g, b].reduce((acc, cur) => {
