68ac236348480e954393a6749a573fc07c7dfe8a
Ken Vega <ken.vega.g@gmail.com>
2020-01-24 15:53:11 -0500

    update tickets solution

--- src/0035_tickets.js
+++ src/0035_tickets.js
@@ -1,3 +1,14 @@
+// level 6kyu
+// https://www.codewars.com/kata/555615a77ebc7c2c8a0000b8
+
+// There are a lot of people at the cinema box office standing in a huge line.
+// Each of them has a single 100, 50 or 25 dollar bill. An "Avengers" ticket costs 25 dollars.
+// Vasya is currently working as a clerk. He wants to sell a ticket to every single person in this line.
+
+// Can Vasya sell a ticket to every person and give change if he initially has no money and sells the tickets strictly in the order people queue?
+// Return YES, if Vasya can sell a ticket to every person and give change with the bills he has at hand at that moment.
+// Otherwise return NO.
+
 function tickets(peopleInLine) {
   const dollarsCount = { 25: 0, 50: 0, 100: 0 };
 
