15216cb867c6d3a064ec8ce2fbcb80cd536fcd0e
Ken Vega <ken.vega.g@gmail.com>
2026-01-29 11:31:25 -0500

    master - add best O(n) solution to twoSum

--- src/leetcode/js/0001-easy-twoSum.js
+++ src/leetcode/js/0001-easy-twoSum.js
@@ -45,7 +45,7 @@ console.log(twoSum([3, 3], 6)) // [0,1]
 console.log(twoSum([2, 5, 5, 11], 10)) // [1,2]
 
 // when going number by number if I find in the array the other number I need then just return that
-// after solution v2: this doesn't really goes from On^2 to On because indexOf also is traversing all the array
+// after solution v2: this doesn't really goes from On^2 to On because indexOf is also traversing all the array
 function twoSumv2(nums, target) {
   let num = 0
   let other = 0
@@ -69,17 +69,58 @@ console.log(twoSumv2([3, 2, 4], 6)) // [1,2]
 console.log(twoSumv2([3, 3], 6)) // [0,1]
 console.log(twoSumv2([2, 5, 5, 11], 10)) // [1,2]
 
-// if we start saving the numbers we already saw in a map then we can stop doing a On^2
-function twoSumv3(nums, target) {}
+// if we save the numbers we already saw in a Map then we can stop doing a O(n^2)
+// because if later we know we need a specific number we already know where that number is in the Map
+// it could be a Map or an object like 'seen'
+// the important part that improves the time is the lookup in the object
+//   which is a faster lookup than indexOf in an array
+function twoSumv3(nums, target) {
+  let num = 0
+  let other = 0
+  const seen = {}
+
+  for (let i = 0; i < nums.length; i++) {
+    num = nums[i]
+    other = target - num
+
+    if (seen[other] !== undefined) {
+      return [seen[other], i]
+    }
+
+    seen[num] = i
+  }
+}
 
 console.log(twoSumv3([2, 7, 11, 15], 9)) // [0,1]
 console.log(twoSumv3([3, 2, 4], 6)) // [1,2]
 console.log(twoSumv3([3, 3], 6)) // [0,1]
 console.log(twoSumv3([2, 5, 5, 11], 10)) // [1,2]
 
+// solution with a Map
+function twoSumv4(nums, target) {
+  const seen = new Map()
+
+  for (let i = 0; i < nums.length; i++) {
+    const x = nums[i]
+    const need = target - x
+
+    if (seen.has(need)) {
+      return [seen.get(need), i]
+    }
+
+    seen.set(x, i)
+  }
+}
+
+console.log(twoSumv4([2, 7, 11, 15], 9)) // [0,1]
+console.log(twoSumv4([3, 2, 4], 6)) // [1,2]
+console.log(twoSumv4([3, 3], 6)) // [0,1]
+console.log(twoSumv4([2, 5, 5, 11], 10)) // [1,2]
+
 // to remember
-//  when you have a On^2 in time and 1 in space
-//  you usually can transform it to a On in time and On in space
+//  when you have a O(n^2) in time and O(1) in space
+//  you usually can transform it to a O(n) in time and O(n) in space
+//  we generally prefer to improve time because it is easy to increase the space in memory
 
 // array.indexOf(theElementYouLookFor, theIndexWhereToStartLookingFor)
 
