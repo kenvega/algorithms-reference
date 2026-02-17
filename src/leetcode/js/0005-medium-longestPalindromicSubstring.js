12bdc841cd4eb418f46dc2656599522468e0391e
Ken Vega <ken.vega.g@gmail.com>
2026-02-17 11:55:53 -0500

    master - add 0009

--- src/leetcode/js/0005-medium-longestPalindromicSubstring.js
+++ src/leetcode/js/0005-medium-longestPalindromicSubstring.js
@@ -1,18 +1,23 @@
 // medium
 // https://leetcode.com/problems/longest-palindromic-substring/description/
-// Given a string s, return the longest palindromic substring in s.
+
+// Given a string 's', return the longest palindromic substring in 's'
+// palindromic: it reads the same forward and backward
 
 // Example 1:
-
 // Input: s = "babad"
 // Output: "bab"
 // Explanation: "aba" is also a valid answer.
-// Example 2:
 
+// Example 2:
 // Input: s = "cbbd"
 // Output: "bb"
 
 // Constraints:
-
 // 1 <= s.length <= 1000
 // s consist of only digits and English letters.
+
+function longestPalindrome(string) {}
+
+console.log(longestPalindrome("babad")) // "bab"
+console.log(longestPalindrome("cbbd")) // "bb"
