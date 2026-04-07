34e64ae0315e6487f69a35d1ad167fc0fb48ae26
Ken Vega <ken.vega.g@gmail.com>
2026-04-07 17:35:19 -0500

    update 0005 a bit

--- src/leetcode/js/0005-medium-longestPalindromicSubstring.js
+++ src/leetcode/js/0005-medium-longestPalindromicSubstring.js
@@ -24,25 +24,20 @@
 //  to account for even palindromes add a special character between every character
 //    that way we always can find palindromes (odd or even)
 function longestPalindrome(string) {
-  if (string.length <= 2) {
-    if (string[0] !== string[1]) {
-      return string[0]
-    }
-    return string
-  }
+  if (string.length < 2) return string
+  if (string.length === 2) return string[0] === string[1] ? string : string[0]
 
-  string = string.split("").join("#").concat("#")
+  string = `#${string.split("").join("#")}#`
 
-  let longestPalindromeFound = string[0]
+  let longestPalindromeFound = ""
   let currentPalindromeFound = ""
 
-  for (let i = 0; i < string.length - 1; i++) {
-    // for each character of the string (which already includes the special character '#' to account for even palindromes)
-    //   start to expand from each character and check if you have a palindrome
-    //     stop if you reach the limits of the string or you don't get a palindrome when expanding
-    //     once you stop, check if the palindrome you found is the biggest and update it
-    // then go for the next character and start again until you do the same for the last character
-
+  // for each character of the string (which already includes the special character '#' to account for even palindromes)
+  //   start to expand from each character and check if you have a palindrome
+  //     stop if you reach the limits of the string or you don't get a palindrome when expanding
+  //     once you stop, check if the palindrome you found is the biggest and update it
+  // then go for the next character and start again until you do the same for the last character
+  for (let i = 1; i < string.length - 1; i++) {
     let j = 1
 
     while (true) {
@@ -57,10 +52,8 @@ function longestPalindrome(string) {
         break
       }
 
-      // at this point you have a palindrome
-      if (string[i - j] === string[i + j]) {
-        currentPalindromeFound = string.slice(i - j, i + j + 1)
-      }
+      // at this point you have a palindrome because you already did the check
+      currentPalindromeFound = string.slice(i - j, i + j + 1)
 
       j++
     }
@@ -71,6 +64,7 @@ function longestPalindrome(string) {
 
 console.log(longestPalindrome("ac")) // "a"
 console.log(longestPalindrome("abb")) // "bb"
+console.log(longestPalindrome("aba")) // "aba"
 console.log(longestPalindrome("cbabd")) // "bab"
 console.log(longestPalindrome("babad")) // "bab"
 console.log(longestPalindrome("cbbd")) // "bb"
