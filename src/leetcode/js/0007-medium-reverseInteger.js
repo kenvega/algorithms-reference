1baf33c9dd33b14992adb28992a82adebe120e38
Ken Vega <ken.vega.g@gmail.com>
2026-04-20 14:01:12 -0500

    add 0007

---
+++ src/leetcode/js/0007-medium-reverseInteger.js
@@ -0,0 +1,59 @@
+// medium
+// https://leetcode.com/problems/reverse-integer/
+
+// Given a signed 32-bit integer x
+// return x with its digits reversed
+
+// If reversing x causes the value to go outside
+//   the signed 32-bit integer range [-2^31, 2^31 - 1], then return 0.
+
+// Assume the environment does not allow you to store 64-bit integers (signed or unsigned).
+
+// Example 1:
+// Input: x = 123
+// Output: 321
+
+// Example 2:
+// Input: x = -123
+// Output: -321
+
+// Example 3:
+// Input: x = 120
+// Output: 21
+
+// Constraints:
+// -2^31 <= x <= 2^31 - 1
+
+// my solution v1
+//  convert the number to an string then array then reverse it and then return it back
+function reverse(number) {
+  const MIN = (-2) ** 31
+  const MAX = 2 ** 31 - 1
+
+  let string
+  if (number < 0) {
+    string = number.toString()
+    string = string.slice(1, string.length)
+    string = string.split("").reverse().join("")
+    number = Number(`-${string}`)
+    if (number < MIN) {
+      return 0
+    }
+
+    return number
+  }
+
+  string = number.toString().split("").reverse().join("")
+  number = Number(string)
+
+  if (number > MAX) {
+    return 0
+  }
+
+  return number
+}
+
+console.log(reverse(123)) // 321
+console.log(reverse(-123)) // -321
+console.log(reverse(120)) // 21
+console.log(reverse(1534236469)) // 0
--- src/leetcode/js/0011-medium-containerWithMostWater.js
+++ src/leetcode/js/0011-medium-containerWithMostWater.js
@@ -25,23 +25,23 @@
 
 // solution v2 (correct approach)
 //  choose the first and last one as the vertical lines to calculate the area
-//  then try to get a larger area by moving the left or the right vertical line
-//  width would be reduced by doing that but we could hopefully get a bigger vertical line
-//  to decide which vertical line to move just move the shorter one
-//  because moving the larger line could be even taller but in that best case the shorter one still defines the vertical area
-//  then loop doing that until the vertical lines meet
-//  TODO: what happens if both vertical heights are the same at some point?
+//    then try to get a larger area by moving the left or the right vertical line
+//      width would be reduced by doing that but we could hopefully get a bigger vertical line
+//    to decide which vertical line to move just move the shorter one 1 step near to the other one
+//      because moving the larger vertical line could get even a taller one but in that best case scenario
+//        the shorter one would still define the max vertical height for the area
+//      what happens if both vertical heights are the same at some point?
+//        move any because the immediate next area moving either vertical line
+//           will always be smaller than the area where the two vertical lines are equal height
+//             because width will be smaller and the min height will be the same as before or lower
+//    then loop doing that until the vertical lines basically meet each other
 function maxAreav2(heights) {
-  if (heights.length === 2) {
-    return Math.min(heights[0], heights[1])
-  }
-
   let maxAreaRecorded = 0
 
   let left = 0
   let right = heights.length - 1
 
-  while (right - left > 1) {
+  while (right - left >= 1) {
     const shorterVerticalLine = Math.min(heights[left], heights[right])
     const currentArea = (right - left) * shorterVerticalLine
 
@@ -60,7 +60,7 @@ function maxAreav2(heights) {
 }
 
 console.log(maxAreav2([1, 1])) // 1
-console.log(maxAreav2([8, 7, 2, 1])) // 7 // TODO: fix this case. should return 7 but it's returning 4
+console.log(maxAreav2([8, 7, 2, 1])) // 7
 console.log(maxAreav2([1, 8, 6, 2, 5, 4, 8, 3, 7])) // 49
 
 // solution v1 (too slow because of O(n^2) time. not accepted as solution in leetcode)
